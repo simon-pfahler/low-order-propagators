874915fa4611388929c6bbe24d1867ba24f6bbd5
simon-pfahler <simon@pfahler.online>
2026-09-10 08:43:09 +0200

    Increase mass region we investigate

--- scripts/get_residuals_hopping.py
+++ scripts/get_residuals_hopping.py
@@ -28,9 +28,9 @@ nsteps = int(args["--nsteps"])
 vol = args["--volume"]
 Lx, Lt = map(int, args["--volume"].split("c"))
 lattice_size = [Lx, Lx, Lx, Lt]
-if vol=="8c16":
-    test_configs = [1000,2000,2347,786]
-elif vol=="16c32":
+if vol == "8c16":
+    test_configs = [1000, 2000, 2347, 786]
+elif vol == "16c32":
     test_configs = [282, 472, 329, 591]
 else:
     raise ValueError(f"Volume {vol} not supported!")
@@ -40,6 +40,19 @@ print(
     f"Calculating residuals for hopping expansion with {nsteps} steps,"
     f"mass={mass:.2f}, volume {args['--volume']}"
 )
+
+# Handle m=-4 case:
+if mass == -4:
+    # Ensure output directory exists
+    os.makedirs("data/residuals", exist_ok=True)
+
+    residuals = torch.nan * torch.ones(4, 100)
+
+    # Save output
+    torch.save(
+        residuals,
+        f"data/residuals/residuals_{nsteps}steps_{vol}_hopping_m{mass:.2f}.pt",
+    )
 
 test_Us = [
     torch.load(get_config_path(lattice_size, c), weights_only=True)
@@ -76,9 +89,9 @@ for sample_idx in range(100):
     v = torch.randn(*lattice_size, 4, 3, dtype=torch.cdouble)
     v /= qcd_ml.util.linear_algebra.norm(v)
     for test_idx, w in enumerate(test_ws):
-        residuals[test_idx, sample_idx] = residual(
-            w, lambda x: hopping_expansion(x, w), v.clone()
-        )**2
+        residuals[test_idx, sample_idx] = (
+            residual(w, lambda x: hopping_expansion(x, w), v.clone()) ** 2
+        )
 
 # Ensure output directory exists
 os.makedirs("data/residuals", exist_ok=True)
