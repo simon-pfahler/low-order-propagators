f61b872bbfc1d778b68caed399bf672278b2d210
simon-pfahler <simon@pfahler.online>
2026-09-10 10:20:09 +0200

    Add script to analyze residuals variance

---
+++ scripts/analyze_residual_variance.py
@@ -0,0 +1,20 @@
+import os
+
+import torch
+
+filenames = os.listdir(f"data/residuals/")
+filenames = [f for f in filenames if "hopping" not in f]
+
+Qs = torch.stack(
+    [torch.load(f"data/residuals/{f}", weights_only=True) for f in filenames]
+)
+
+variances = Qs.var(dim=(1, 2))
+means = Qs.mean(dim=(1, 2))
+relerrs = torch.sqrt(variances) / means
+
+max_relerr_idx = torch.argmax(relerrs)
+
+print(f"Maximum relative error observed for {filenames[max_relerr_idx]}")
+print(f"This file has a relative error of {relerrs[max_relerr_idx]}")
+print(f"The average relative error across all calculations is {relerrs.mean()}")
